fac8745f62bb11015ed6010cf0a5ba5429420794
SlavikKhrapach <hrapachslavik@gmail.com>
2023-05-08 22:49:32 -0400

    Set up routs and data transfer

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<!--
-Author: Slavik Khrapach
-Date: 4/22/2023
-Description: This file turns on error reporting, Fat-Free, and creates a view
--->
 <?php
+/*
+ * Slavik Khrapach
+ * 4/8/2023
+ * 328/application/index.php
+ * Controller for application project
+ *
+ */
 
 //Turn on error reporting
 ini_set('display_errors', 1);
@@ -19,35 +21,81 @@ $f3 = Base::instance();
 $f3->route('GET /', function() {
     $view = new Template();
 
-    //echo "<h1>TEST</h1>";
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /personal', function() {
+$f3->route('GET|POST /personal', function($f3) {
 
-    // Test
-    //echo "<h1>TEST</h1>";
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+        // Get data
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $phone = $_POST['phone'];
+
+        // Store data in the session array
+        $f3->set('SESSION.fname', $fname);
+        $f3->set('SESSION.lname', $lname);
+        $f3->set('SESSION.email', $email);
+        $f3->set('SESSION.state', $state);
+        $f3->set('SESSION.phone', $phone);
+
+        // Redirect to experience form page
+        $f3->reroute('experience');
+    }
     // Display view page
     $view = new Template();
     echo $view->render('views/personalInfoForm.html');
 });
 
-$f3->route('GET /experience', function() {
+$f3->route('GET|POST /experience', function($f3) {
 
-    // Test
-    //echo "<h1>TEST</h1>";
+    // If the form is posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+        // Get data
+        $bio = $_POST['biography'];
+        $git = $_POST['gitLink'];
+        $yrExp = $_POST['years-experience'];
+        $relocate = $_POST['relocate'];
+
+        // Store data in the session array
+        $f3->set('SESSION.bio', $bio);
+        $f3->set('SESSION.git', $git);
+        $f3->set('SESSION.yrExp', $yrExp);
+        $f3->set('SESSION.relocate', $relocate);
+
+
+        // Redirect to openings form page
+        $f3->reroute('openings');
+    }
     // Display view page
     $view = new Template();
     echo $view->render('views/experienceForm.html');
 });
 
-$f3->route('GET /openings', function() {
+$f3->route('GET|POST /openings', function($f3) {
 
-    // Test
-    //echo "<h1>TEST</h1>";
+    // If the form is posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+        // Get data
+        $sdj = implode(", ", $_POST['sdj']);
+        $indV = implode(", ", $_POST['indV']);
+
+
+
+        // Store data in the session array
+        $f3->set('SESSION.sdj', $sdj);
+        $f3->set('SESSION.indV', $indV);
+
+
+        // Redirect to openings form page
+        $f3->reroute('summary');
+    }
     // Display view page
     $view = new Template();
     echo $view->render('views/openingsForm.html');
