3daf629cdb513df1a62109ee484aaa2a5320e41e
SlavikKhrapach <hrapachslavik@gmail.com>
2023-05-21 23:38:22 -0400

    Added data layer file

---
+++ model/data-layer.php
@@ -0,0 +1,16 @@
+<?php
+
+function getExperience() {
+    $experience = array("0-2", "2-4", "4+");
+    return $experience;
+}
+
+function getJobs() {
+    $jobs = array("Javascript", "HTML", "PHP", "CSS", "Java", "ReactJS", "Python", "NodeJs");
+    return $jobs;
+}
+
+function getVerticals() {
+    $verticals = array("Saas", "Industrial tech", "Health tech", "Cybersecurity", "Ag tech", "HR tech");
+    return $verticals;
+}
